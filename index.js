3a26c745d72300beecadb9e95cb12365bc64864c
Rupam Das <rejisterjack@gmail.com>
2025-02-24 18:47:42 +0530

    feat: lib and models

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const express = require('express');
-const { resolve } = require('path');
+const express = require("express")
+const { resolve } = require("path")
+const cors = require("cors")
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
-app.use(express.static('static'));
+app.use(cors())
+app.use(express.json())
+app.use(express.static("static"))
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
-});
+app.get("/", (req, res) => {
+  res.sendFile(resolve(__dirname, "pages/index.html"))
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+  console.log(`Example app listening at http://localhost:${port}`)
+})
---
+++ models/Book.js
@@ -0,0 +1,16 @@
+const { sequelize, DataTypes } = require("../lib/sequelize")
+const Author = require("./Author")
+const Genre = require("./Genre")
+
+const Book = sequelize.define("Book", {
+  title: DataTypes.STRING,
+  description: DataTypes.TEXT,
+  publicationYear: DataTypes.INTEGER,
+  authorId: DataTypes.INTEGER,
+})
+
+Book.belongsTo(Author, { foreignKey: "authorId" })
+Book.belongsToMany(Genre, { through: "BookGenres" })
+Genre.belongsToMany(Book, { through: "BookGenres" })
+
+module.exports = Book
